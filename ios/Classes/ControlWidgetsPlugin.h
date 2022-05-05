a4aaa253ec400f947652a3dd7b597a567e335971
RomanBase <ro.hornak@gmail.com>
2022-05-05 18:48:47 +0200

    minor changes

--- ios/Classes/ControlWidgetsPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface ControlWidgetsPlugin : NSObject<FlutterPlugin>
-@end
--- lib/widgets.dart
+++ lib/widgets.dart
@@ -4,7 +4,6 @@ import 'dart:math' as math;
 import 'dart:ui' as ui;
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter_control/core.dart';
 
 part 'clipper/rounded_convex_border.dart';
